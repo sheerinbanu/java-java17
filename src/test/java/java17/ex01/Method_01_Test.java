830a711a3378705acece334107cbbac274e4ec41
Sheerin Banu <sheemuzz@outlook.com>
2024-11-27 15:08:59 +0100

    Default method exo done

--- src/test/java/java17/ex01/Method_01_Test.java
+++ src/test/java/java17/ex01/Method_01_Test.java
@@ -18,6 +18,7 @@ public class Method_01_Test {
         List<Person> findAll();
 
         // TODO créer une méthode int sumAge()
+        int sumAge();
         // TODO Cette méthode retourne le résultat de l'addition des ages des personnes
     }
     // end::IDao[]
@@ -30,6 +31,15 @@ public class Method_01_Test {
         public List<Person> findAll() {
             return people;
         }
+
+        @Override
+        public int sumAge() {
+            int sum = 0;
+            for (Person p : people) {
+                sum += p.getAge();
+            }
+            return sum;
+        }
     }
 
     class DaoB implements IDao {
@@ -40,6 +50,15 @@ public class Method_01_Test {
         public List<Person> findAll() {
             return people;
         }
+
+        @Override
+        public int sumAge() {
+            int sum = 0;
+            for (Person p : people) {
+                sum += p.getAge();
+            }
+            return sum;
+        }
     }
 
     @Test
@@ -48,7 +67,7 @@ public class Method_01_Test {
         DaoA daoA = new DaoA();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoA.sumAge();
 
         assert result == 210;
     }
@@ -59,7 +78,8 @@ public class Method_01_Test {
         DaoB daoB = new DaoB();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoB.sumAge();
+
 
         assert result == 5050;
 
